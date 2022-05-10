e7dc30ddd5cf331bb51ae0a5ab3d27a9ba5eb7f3
guchvlado <guchvlado324@gmail.com>
2022-05-10 18:50:38 +0300

    adantages, ourWork, agent, price sections

---
+++ src/blocks/modules/agent/agent.js
@@ -0,0 +1,13 @@
+modules.define('agent', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
