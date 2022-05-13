dcf451ba5c3a8aafca5e3f9bfc710a675118806a
guchvlado <guchvlado324@gmail.com>
2022-05-13 14:53:23 +0300

    slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
 import "./import/modules";
 import "./import/components";
+import Swiper, { Navigation } from "swiper";
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation],
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 45,
+    speed: 1500,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+        }
+    },
+});
